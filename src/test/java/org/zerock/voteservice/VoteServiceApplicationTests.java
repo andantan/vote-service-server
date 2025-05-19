2d172e98597aa6b6d0ccb7492888d4b1834cc791
andantan <kyubin2892@gmail.com>
2025-05-19 19:16:57 +0900

    .gitignore configuration

---
+++ src/test/java/org/zerock/voteservice/VoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.voteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
