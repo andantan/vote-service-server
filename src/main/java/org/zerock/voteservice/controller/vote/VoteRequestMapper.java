0195eaf18094a568ec7daa6614bbaedd01c104c3
andantan <kyubin2892@gmail.com>
2025-06-08 20:45:53 +0900

    Deploy Cors security configuration mapping & domain to ip [ localhost -> 127.0.0.1 ]

--- src/main/java/org/zerock/voteservice/controller/vote/VoteRequestMapper.java
+++ src/main/java/org/zerock/voteservice/controller/vote/VoteRequestMapper.java
@@ -1,9 +1,9 @@
 package org.zerock.voteservice.controller.vote;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vote")
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 public abstract class VoteRequestMapper {
 }
