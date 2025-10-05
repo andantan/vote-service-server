ad4ca125a5d4fbfcf761a27855634341badcef3a
andantan <kyubin2892@gmail.com>
2025-10-06 02:42:51 +0900

    Feature: invoking Scheduler to user_verification by expiration

---
+++ src/main/java/org/zerock/voteservice/adapter/out/jpa/scheduler/VerificationCleanupScheduler.java
@@ -0,0 +1,26 @@
+package org.zerock.voteservice.adapter.out.jpa.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.zerock.voteservice.adapter.out.jpa.repository.UserVerificationRepository;
+
+import java.util.Date;
+
+@Log4j2
+@Component
+@RequiredArgsConstructor
+public class VerificationCleanupScheduler {
+
+    private final UserVerificationRepository userVerificationRepository;
+
+    @Scheduled(cron = "0 */10 * * * *")
+    public void cleanupExpiredVerification() {
+        log.info("[Scheduler-cron] Starting cleanup task for expired verification data...");
+
+        int deletedCount = userVerificationRepository.deleteByExpirationBefore(new Date());
+
+        log.info("[Scheduler-cron] Successfully deleted {} expired verification records.", deletedCount);
+    }
+}
